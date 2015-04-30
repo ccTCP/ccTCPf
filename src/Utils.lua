a11143aea45fccfa71f5cf98e3d6690751759c77
cmdpwnd <cmdstack@gmail.com>
2015-04-29 23:13:28 -0500

    Update Utils.lua
    added toHex(num) function

--- src/Utils.lua
+++ src/Utils.lua
@@ -22,4 +22,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
-]]
+--]]
+
+function toHex(num)
+	local hexTemp = '0123456789abcdef'
+	 hex = ''
+	while num > 0 do
+		local mod = math.fmod(num, 16)
+		hex = string.sub(hexTemp, mod+1, mod+1) .. hex
+		num = math.floor(num / 16)
+	end
+	if hex == '' then hex = '0' end
+	return hex
+end
+

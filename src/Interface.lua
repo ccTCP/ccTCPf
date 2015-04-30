8a880ddc7f798cdd6cb5886e1a2a11b2f0ea44f1
cmdpwnd <cmdstack@gmail.com>
2015-04-29 22:44:51 -0500

    Update Interface.lua
    variable localization

--- src/Interface.lua
+++ src/Interface.lua
@@ -30,7 +30,7 @@ THE SOFTWARE.
 int = {}
 
 function int.createMac(side)
-	sideNum = {top = 0,bottom = 1,left = 2,right = 3,back = 4}
+	local sideNum = {top = 0,bottom = 1,left = 2,right = 3,back = 4}
 	local mac = os.computerID() * math.random(1,200) + sideNum[side]
 	return mac
 end

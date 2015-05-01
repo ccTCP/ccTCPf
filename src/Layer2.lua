bfa98f1864710d023c828a7892930af829937b34
TheOnlyCreator <wassil.janssen@gmail.com>
2015-05-02 00:44:01 +0300

    Layer2: fised

--- src/Layer2.lua
+++ src/Layer2.lua
@@ -32,8 +32,9 @@ int = {}
 
 --L2 functions
 function int.createMac(side)
-	if side then
-		local macBuffer = tostring(utils.toHex(os.computerID() * 6 + {top = 0,bottom = 1,left = 2,right = 3,back = 4,front = 5}[side]))
+	sideTable = {top = 0,bottom = 1,left = 2,right = 3,back = 4,front = 5}
+	if side and sideTable[side] then
+		local macBuffer = tostring(utils.toHex(os.computerID() * 6 + sideTable[side]))
 		return string.rep("0",12-#macBuffer).. macBuffer
 	end
 	return false

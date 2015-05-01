dae47b891c677548bd3367a1e218bf89d67c8350
InDieTasten <indietasten@gmail.com>
2015-05-02 00:30:04 +0200

    Event_Hook.lua: added all functionality

--- src/Event_Hook.lua
+++ src/Event_Hook.lua
@@ -23,3 +23,28 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 ]]
+local listeners = {}
+local lastkey = 0
+function insertListener(func)
+    listeners[lastkey+1] = func
+    lastkey = lastkey + 1
+    return lastkey -1
+end
+function removeListener(index)
+    listeners[index] = nil
+end
+
+local ospulleventbackup
+function activate()
+    ospulleventbackup = os.pullEvent
+    os.pullEvent = function(...)
+        local result = {ospulleventbackup(...)}
+        for k,v in pairs(listeners) do
+            v(unpack(result))
+        end
+        return unpack(result)
+    end
+end
+function deactivate()
+    os.pullEvent = ospulleventbackup
+end

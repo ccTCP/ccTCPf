3172f2f34de2731b02c1b841ceff190569cb246d
Creator <wassil.janssen@gmail.com>
2015-05-14 22:48:11 +0300

    tcp: working

--- tcp.lua
+++ tcp.lua
@@ -14,44 +14,16 @@ local Utils = {}
 --Function
 
 local function requireAPI(_sPath)
-	local sName = fs.getName( _sPath )
-	local tEnv = {}
+	local sName, tEnv = fs.getName( _sPath ), {}
 	setmetatable( tEnv, { __index = _G } )
 	local fnAPI, err = loadfile( _sPath )
 	if fnAPI then
 		setfenv( fnAPI, tEnv )
 		local ok, err = pcall( fnAPI )
-		if not ok then
-			printError( err )
-			return false
-		end
+		if not ok then printError( err ) return false end
 	else
-		printError( err )
-		return false
+		printError( err ) return false
 	end
-	
-	local tAPI = {}
-	for k,v in pairs( tEnv ) do
-		tAPI[k] =  v
-	end
-	return tAPI	
-end(_sPath)
-	local sName = fs.getName( _sPath )
-	local tEnv = {}
-	setmetatable( tEnv, { __index = _G } )
-	local fnAPI, err = loadfile( _sPath )
-	if fnAPI then
-		setfenv( fnAPI, tEnv )
-		local ok, err = pcall( fnAPI )
-		if not ok then
-			printError( err )
-			return false
-		end
-	else
-		printError( err )
-		return false
-	end
-	
 	local tAPI = {}
 	for k,v in pairs( tEnv ) do
 		tAPI[k] =  v
@@ -60,5 +32,9 @@ end(_sPath)
 end
 
 local function loadModules()
-
+	for i,v in pairs(fs.list(dir)) do
+		print(v)
+	end
 end
+
+loadModules()

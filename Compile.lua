edfe8029c8a6a58a2ad54f9586cbca2261e1f2ec
Creator <wassil.janssen@gmail.com>
2015-05-19 21:59:43 +0300

    Compile: working

--- Compile.lua
+++ Compile.lua
@@ -5,24 +5,50 @@
 --Variables
 local files = fs.list("ccTCPf/src")
 local t = {}
+local ending = [[
+function loadAPI(func)
+	local tEnv = {}
+	setmetatable( tEnv, { __index = _G } )
+    setfenv(func, tEnv )
+    local ok, err = pcall(func)
+    if not ok then
+        printError( err )
+        return false
+    end
+    printError( err )
+    return false
+
+    local tAPI = {}
+    for k,v in pairs( tEnv ) do
+        tAPI[k] =  v
+    end
+
+    return tAPI
+end
+
+
+for i,v in pairs(t) do
+
+end]]
 
 
 --Functions
 local function createFile(path)
-	local file = fs.open(path)
+	local file = fs.open(path,"r")
 	local data = file.readAll()
 	file.close()
-	local name = fs.getName(path)
+	local name = string.match(fs.getName(path),"[^%.]+")
+	print(name)
 	local header = "function "..name.."()"
 	local footer = "end"
 	local endResult = header.."\n"..data.."\n"..footer
-	return func = endResult
+	return endResult
 end
 
 for i,v in pairs(files) do
-	t[v] = createFile("ccTCPf/src".."/"..v)
+	t[v] = createFile("ccTCPf/src/"..v)
 end
 
 local file = fs.open("well","w")
-file.write(textutils.serialize(t))
+file.write("t = "..textutils.serialize(t).."\n")
 file.close()

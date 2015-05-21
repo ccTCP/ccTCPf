97cc5351bb94ba598bb8485919ef83507d4113e3
cmdpwnd <cmdstack@gmail.com>
2015-05-20 23:07:47 -0500

    ipv4_Debugging

--- src/IPv4.lua
+++ src/IPv4.lua
@@ -60,7 +60,7 @@ function getNetworkAddress2(addr)
   local binAddr = getBinaryAddress(addr:sub(1,-4))
   print(binAddr)
   local binAddrOctet = {binAddr:sub(1,8),binAddr:sub(9,16),binAddr:sub(17,24),binAddr:sub(25,32)}
-  print(binAddrOctet[1].."\n"..binAddrOctet[2].."\n"..binAddrOctet[3].."\n"..binAddrOctet[4])
+  print("\n"..binAddrOctet[1].."\n"..binAddrOctet[2].."\n"..binAddrOctet[3].."\n"..binAddrOctet[4])
   local binMask = cidrDecTbl[addr:sub(-2,-1)]
   print(binMask)
   local binMaskOctet = {binMask:sub(1,8),binMask:sub(9,16),binMask:sub(17,24),binMask:sub(25,32)}

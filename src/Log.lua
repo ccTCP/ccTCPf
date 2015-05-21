3f382632a57af08de41d9b924d09007cc8bd81b8
cmdpwnd <cmdstack@gmail.com>
2015-05-20 21:06:42 -0500

    fixed log.lua func reference

--- src/Log.lua
+++ src/Log.lua
@@ -4,15 +4,16 @@
 	for TheOS
 ]]--
 
-function log(destination,message,app)
+args = {...}
+--args[1,2,3] represent: destination,message,app
+
 	if not doLog then return end
 	local finalMsg = "["..os.day()..":"..os.time().."]"
-	if app then
-		finalMsg = finalMsg..".["..tostring(app).."]:"..tostring(message)
+	if args[3] then
+		finalMsg = finalMsg..".["..tostring(args[3]).."]:"..tostring(args[2])
 	else
-		finalMsg = finalMsg..":"..tostring(message)
+		finalMsg = finalMsg..":"..tostring(args[2])
 	end
-	local m = fs.open(config.dir.."Logs/"..destination..".log","a")
+	local m = fs.open(config.dir.."Logs/"..args[1]..".log","a")
 	m.write(finalMsg.."\n")
 	m.close()
-end

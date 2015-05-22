9ff70e59ea9f1c4c922809f21395baa8a4275461
cmdpwnd <cmdstack@gmail.com>
2015-05-21 21:54:49 -0500

    ipv4 almost done for getNet

--- src/IPv4.lua
+++ src/IPv4.lua
@@ -70,7 +70,7 @@ function getNetworkAddress2(address)
   local mask = cidrDecTbl[tonumber(cidr)]
   local binMask = getBinaryAddress(mask)
   local binMaskOctet = {binMask:sub(1,8),binMask:sub(9,16),binMask:sub(17,24),binMask:sub(25,32)}
-  
+  print(bit.band(binMask,binAddr))
 end
 
 function getAddressInfo(address)

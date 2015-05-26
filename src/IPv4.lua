a02980ec43e691c7fa353aab0c51a5248fde7e7e
cmdpwnd <cmdstack@gmail.com>
2015-05-26 02:00:59 -0500

    IPv4: 99% DONE: getAddressInfo()

--- src/IPv4.lua
+++ src/IPv4.lua
@@ -191,7 +191,7 @@ hostAddr#
       return vars[rtnAddr]
   else
     if index == "all" then
-      print("all")
+      return vars[rtnAddr][1],vars[rtnAddr][2],vars[rtnAddr][3],vars[rtnAddr][4]
     else
       return vars[rtnAddr][index]
     end 

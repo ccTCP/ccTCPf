801d1a4c2cfc367e2e0ff996fa6f76e765397e72
Creator <wassil.janssen@gmail.com>
2016-01-04 23:53:08 +0200

    Just synced progress

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,16 @@
 --[[
-	The file that is excected to load ccTCP.
+	Script: ccTCP loader.
 ]]--
+
+local ccTCPfPath = "ccTCPf/src/"
+
+local dofile = function(path)
+	local file = fs.open(ccTCPfPath..path,"r")
+	local data = file.readAll()
+	file.close()
+	local func, err = loadstring(data)
+end
+
+local Internal = {}
+
+local API = {} -- Outside interface.

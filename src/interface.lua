b5274ae392336f00d7d49b63cc43c92093384f7d
cmdpwnd <cmdstack@gmail.com>
2016-01-03 20:04:44 -0600

    debugging interface.tInterface table -- did work, doesn't now?

--- src/interface.lua
+++ src/interface.lua
@@ -1,13 +1,13 @@
 --interface.lua
 
-tInterfaceType = {}
-tInterfaceWrap = {}
-tInterfaceState = {}
 channel = 2016
 
-local function _init_()
+function _init_()
 	if not _INTERFACE_RUN_STATUS then 
-		_INTERFACE_RUN_STATUS=1
+		--print('interface.init = active')
+		_G._INTERFACE_RUN_STATUS=1
+		tInterface = {}
+		tInterface.count = 0
 		wrap()
 		return 
 	end
@@ -15,21 +15,29 @@ end
 
 function wrap()
 	local peripherals = peripheral.getNames()
+	--print(#peripherals)
 	for i=1,#peripherals do
-		if not (peripheral.getType(peripherals[i])=="modem") then i=i+1 else
+		if not (peripheral.getType(peripherals[i])=="modem") then --print('index: '..peripherals[i]..' skipped') ; i=i+1 else
+			tInterface.count = tInterface.count+1
 			local index = peripherals[i]
-			tInterfaceWrap[index] = peripheral.wrap(index)
-			tInterfaceWrap[index].ingressQueue = {}
-			tInterfaceWrap[index].egressQueue = {}
-			if tInterfaceWrap[index].isWireless() then
-				tInterfaceType[index] = 'wireless' 
+			--print('index: '..index)
+			tInterface[index] = {}
+			tInterface[index].wrap = peripheral.wrap(index)
+			tInterface[index].ingressQueue = {'_'}
+			tInterface[index].egressQueue = {'_'}
+			if tInterface[index].wrap.isWireless() then
+				tInterface[index].type = 'wireless' 
+				--print(tInterface[index].type)
 			else
-				tInterfaceType[index] = 'wired'
+				tInterface[index].type = 'wired'
+				--print(tInterface[index].type)
 			end
-			if tInterfaceWrap[index].isOpen(channel) then 
-				tInterfaceState[index] = 'up'
+			if tInterface[index].wrap.isOpen(channel) then 
+				tInterface[index].state = 'up'
+				--print(tInterface[index].state)
 			else 
-				tInterfaceState[index] = 'administratively down'
+				tInterface[index].state = 'administratively down'
+				--print(tInterface[index].state)
 			end
 		end
 	end
@@ -37,43 +45,41 @@ function wrap()
 end
 
 function shut(interface)
-	tInterfaceWrap[interface].close(channel)
-	tInterfaceState[interface] = 'administratively down'
-	return true
+	tInterface[interface].state = 'administratively down'
+	return tInterface[interface].wrap.close(channel)
 end
 
 function noshut(interface)
-	tInterfaceWrap[interface].open(channel)
-	tInterfaceState[interface] = 'up'
-	return true
+	print(interface)
+	tInterface[interface].state = 'up'
+	return tInterface[interface].wrap.open(channel)
 end
 
 function transmit(interface,...)
-	if not (tInterfaceState[interface] == "up") then return error('Interface down',2) end
+	if not (tInterface[interface].state == "up") then return error('Interface down',2) end
 	local data = {...}
-	tInterfaceWrap[interface].transmit(channel,channel,data)
-	return true
+	return tInterface[interface].wrap.transmit(channel,channel,data)
 end
 
 function receive(...)
 	local interface = {...}
 	if interface[1] == "all" or not interface[1] then
 		local t = {}
-		for i=1,#tInterfaceWrap do
-			if not tInterfaceWrap[i].ingressQueue[1] then i=i+1 else
-				t[tInterfaceWrap[i]] = tInterfaceWrap[i].ingressQueue[1]
-				table.remove(tInterfaceWrap[i].ingressQueue,1)
+		for i=1,#tInterface do
+			if not tInterface[i].ingressQueue[1] then i=i+1 else
+				t[tInterface[i]] = tInterface[i].ingressQueue[1]
+				table.remove(tInterface[i].ingressQueue,1)
 			end
 		end
 		if #t < 1 then return else
-		return t
+		return unpack(t)
 		end
 	else
 		local t = {}
 		for i=1,#interface do
-			if not tInterfaceWrap[interface[i]].ingressQueue[1] then i=i+1 else
-				t[interface[i]] = tInterfaceWrap[i].ingressQueue[1]
-				table.remove(tInterfaceWrap[interface[i]].ingressQueue,1)
+			if not tInterface[interface[i]].ingressQueue[1] then i=i+1 else
+				t[interface[i]] = tInterface[i].ingressQueue[1]
+				table.remove(tInterface[interface[i]].ingressQueue,1)
 			end
 		end
 		if #t < 1 then return else
--- src/pm.lua
+++ src/pm.lua
@@ -3,17 +3,22 @@
 
 function _init_()
 	if not _PM_RUN_STATUS then
-		_PM_RUN_STATUS=1
+		--print('pm.init = active')
+		_G._PM_RUN_STATUS=1
 		return
 	end
 end
 
 function cctcpReceive() --This will evolve to become the core of the switching engine and route processor. Receive is currently the top level init function of this daemon.
 	while true do
-		for i=1,#interface.tInterfaceWrap do
+		--print('inside')
+		--print('wrapNum = '..interface.tInterface.count)
+		for i=1,interface.tInterface.count do
 			local event = {os.pullEventRaw()}
-			if event and event[2] == "modem_message" and event[4] == interface.channel and interface.tInterfaceState[event[3]] == 'up' and event[6] then
-				table.insert(interface.tInterfaceWrap[i].ingressQueue,event[6])
+			--print('pullinside')
+			if event and event[2] == "modem_message" and event[4] == interface.channel and interface.tInterface[event[3]].state == 'up' and event[6] then
+				--print('msg')
+				table.insert(interface.tInterface[i].ingressQueue,event[6])
 			end
 		end
 		coroutine.yield(event)
@@ -26,7 +31,9 @@ local co_cctcpReceive = coroutine.create(cctcpReceive)
 function os.pullEventRaw(sFilter)
   while true do
     local event = {pullEventRaw_Backup()}
+	--print('pullevent')
     if coroutine.status(co_cctcpReceive) == "suspended" then
+	--print('+')
       coroutine.resume(co_cctcpReceive, table.unpack(event))
     end
     --Return any events
